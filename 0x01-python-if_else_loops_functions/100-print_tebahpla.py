798325049b1b3a140954b9d9738867fac3ff39d2
Donpaul Oduor <donpaul.oduordonpaul@gmail.com>
2023-06-06 23:52:29 +0300

    commit

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for char in range(122, 96, -1):
     print("{}".format(chr(char)) if char % 2 == 0
-        else "{}".format(chr(char - 32)), end='')
+          else "{}".format(chr(char - 32)), end='')
