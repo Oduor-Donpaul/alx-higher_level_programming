9c34b8b40ff2907775bcf4c8d520f697a269ab1e
Donpaul Oduor <donpaul.oduordonpaul@gmail.com>
2023-06-06 19:54:42 +0300

    commit

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for i in str:
+        if ord(i) > 96 and ord(i) < 122:
+            upper = chr(ord(i) - 32)
+        else:
+            upper = i
+        print(upper, end='')
+    print()
