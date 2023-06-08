1149d6915c85d3bca75d5346393fda33bd6bcc2d
Donpaul Oduor <donpaul.oduordonpaul@gmail.com>
2023-06-08 16:08:37 +0300

    commit

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-import sys
 if __name__ = "__main__"
+    import sys
     args = len(sys.argv) - 1
     print("{} argument.".format(args) if args == 1
           else "{} arguments:".format(args))
