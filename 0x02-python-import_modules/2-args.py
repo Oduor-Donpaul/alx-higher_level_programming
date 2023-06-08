25b0e80a60c3bf0df4f6466e01f26b540ed4cb40
Donpaul Oduor <donpaul.oduordonpaul@gmail.com>
2023-06-08 16:05:35 +0300

    commit

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 import sys
-args = len(sys.argv) - 1
-print("{} argument.".format(args) if args == 1
-      else "{} arguments:".format(args))
-if args > 0:
-    for i, arg in enumerate(sys.argv[1:], start=1):
-        print("{}: {}".format(i, arg))
+if __name__ = "__main__"
+    args = len(sys.argv) - 1
+    print("{} argument.".format(args) if args == 1
+          else "{} arguments:".format(args))
+    if args > 0:
+        for i, arg in enumerate(sys.argv[1:], start=1):
+            print("{}: {}".format(i, arg))
