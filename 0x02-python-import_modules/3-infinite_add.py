bceed6c9746aa6340b6dd783af800e204febfb6f
Donpaul Oduor <donpaul.oduordonpaul@gmail.com>
2023-06-08 16:11:41 +0300

    commit

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
-import sys
+if __name__ = "__main__":
+    import sys
 
-
-def add():
-    result = 0
-    for i in range(1, len(sys.argv)):
-        result += int(sys.argv[i])
-    return result
-
-
-print(add())
+    def add():
+        result = 0
+        for i in range(1, len(sys.argv)):
+            result += int(sys.argv[i])
+        return result
+    print(add())
