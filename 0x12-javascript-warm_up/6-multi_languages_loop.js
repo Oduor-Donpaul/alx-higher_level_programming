0503d4667b65a7b218f8ce86b0b5f6bfda55f265
Donpaul <123797405+Oduor-Donpaul@users.noreply.github.com>
2024-02-12 08:38:06 +0300

    Delete 0x12-javascript-warm_up directory

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/node
-
-const myArray = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
-
-const myLength = myArray.length;
-
-for (let i = 0; i < myLength; i++) {
-  console.log(myArray[i]);
-}
